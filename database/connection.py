23843df7a649b7765614449b31b078904cd62285
Mihir Thakur <tamihir27@yahoo.com>
2025-01-13 22:35:18 -0700

    Added Database Connection

---
+++ database/connection.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+
+class DatabaseConnection:
+    """Class to connect to MongoDB database"""
+
+    def __init__(self):
+        load_dotenv()
+        self.uri = f"mongodb+srv://{os.getenv("MONGODB_USERNAME")}:{os.getenv("MONGODB_PWD")}@resumecluster.sczyi.mongodb.net/?retryWrites=true&w=majority&appName=ResumeCluster"
+        self.client = None
+
+
+    def connect(self):
+        # Create a new client and connect to the server
+        client = MongoClient(self.uri, server_api=ServerApi('1'))
+
+        # Send a ping to confirm a successful connection
+        try:
+            client.admin.command('ping')
+            print("Pinged your deployment. You successfully connected to MongoDB!")
+        except Exception as e:
+            print(e)
+            client.close()
+
+
+    def disconnect(self):
+        self.client.close()
+        print("Disconnected from MongoDB!")
