067bf7c1516bdf1e3f1cd353e2f96e9e71e57219
Mihir Thakur <tamihir27@yahoo.com>
2025-01-16 00:25:51 -0700

    Saving Resume data to DB

--- database/connection.py
+++ database/connection.py
@@ -11,21 +11,48 @@ class DatabaseConnection:
         load_dotenv()
         self.uri = f"mongodb+srv://{os.getenv("MONGODB_USERNAME")}:{os.getenv("MONGODB_PWD")}@resumecluster.sczyi.mongodb.net/?retryWrites=true&w=majority&appName=ResumeCluster"
         self.client = None
+        self.database = None
+        self.collection = None
 
 
     def connect(self):
         # Create a new client and connect to the server
-        client = MongoClient(self.uri, server_api=ServerApi('1'))
+        self.client = MongoClient(self.uri, server_api=ServerApi('1'))
 
         # Send a ping to confirm a successful connection
         try:
-            client.admin.command('ping')
+            self.client.admin.command('ping')
             print("Pinged database deployment. You successfully connected to MongoDB!")
         except Exception as e:
             print(e)
-            client.close()
+            self.client.close()
+
+    def create_database(self, db_name:str):
+        # Create a new database
+        db = self.client[db_name]
+        print(f"Database {db_name} created successfully!")
+        self.database = db
+    
+    def create_collection(self, db, collection_name:str):
+        #  Create a new collection
+        collection = db[collection_name]
+        print(f"Collection {collection_name} created successfully!")
+        self.collection = collection
+
+    def insert_document(self, document:dict):
+        # Insert a new document
+        res = self.collection.insert_one(document)
+        print(res)
+        print("Document inserted successfully!")
 
 
     def disconnect(self):
         self.client.close()
         print("Disconnected from MongoDB!")
+
+
+
+if __name__ == "__main__":
+    db = DatabaseConnection()
+    db.connect()
+    # db.disconnect()
--- parser/resume_parser.py
+++ parser/resume_parser.py
@@ -6,6 +6,7 @@ import pathlib
 from google import genai
 
 sys.path.insert(1, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from database.connection import DatabaseConnection
 from llm_agent.agent import LLMAgent
 
 
@@ -13,13 +14,14 @@ class ResumeParser:
     """Parse resume to extract relevant information"""
     def __init__(self):
         self.agent = LLMAgent()
+        self.dbClient = DatabaseConnection()
 
     def extract_information(self, filename:str, loc='local') -> dict:
         """Extract information from resume"""
 
         # Read resume file
         filepath = pathlib.Path(__file__).parent.parent / "sample_data" / "resumes" / filename
-        filepath = "../sample_data/resumes/Mihir_Thakur_Resume_CITI_Python.pdf"
+        # filepath = "../sample_data/resumes/Mihir_Thakur_Resume_CITI_Python.pdf"
         try:
             path = pathlib.Path(filepath)
             if not path.exists():
@@ -43,8 +45,32 @@ class ResumeParser:
 
         elif loc == 'cloud':
             pass
+        
+
+    def initialise_db(self):
+        """Initialise database connection"""
+        try:
+            self.dbClient.connect()
+            self.dbClient.create_database("resume_db")
+            self.dbClient.create_collection(self.dbClient.database, "resume_collection")
+        except Exception as e:
+            print(e)
+
+
+    def save_resume_to_db(self, filename:str):
+        """Save resume information into database"""
+        try:
+            print("Initializing Database Connection...")
+            self.initialise_db()
+            resume_data = self.extract_information(filename)[0]
+            # Insert JSON document into database
+            self.dbClient.insert_document(resume_data)
+        except Exception as e:
+            print(e)
+        finally:
+            self.dbClient.disconnect()
 
 
 if __name__ == "__main__":
     parser = ResumeParser()
-    print(parser.extract_information("Mihir_Thakur_Resume_CITI_Python.pdf"))
+    parser.save_resume_to_db("Mihir_Thakur_Resume_CITI_Python.pdf")
